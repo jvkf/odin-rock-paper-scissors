8f20947d7ba55a965c3ea7a1ce9b8904170dcde0
João Vítor Ferraz <jvkferraz@gmail.com>
2023-03-20 22:47:20 -0300

    Adding score track

--- script.js
+++ script.js
@@ -4,11 +4,23 @@ function getComputerChoice() {
   return options[Math.floor(Math.random() * options.length)];
 }
 
-function trackScore(playerResult) {
-  let userScore = 0;
-  let computerScore = 0;
-  if (playerResult === 1) {
-  }
+function getPlayerChoice() {
+  return prompt("Your choice");
+}
+
+let playerScore = 0;
+let pcScore = 0;
+
+function increasePlayerScore() {
+  playerScore += 1;
+}
+
+function increasePcScore() {
+  pcScore += 1;
+}
+
+function updateScore(result) {
+  result === "player wins" ? increasePlayerScore() : increasePcScore();
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -22,32 +34,40 @@ function playRound(playerSelection, computerSelection) {
   switch (pcChoice) {
     case "rock":
       if (playerChoice === "paper") {
+        updateScore("player wins");
         return "You win! Paper beats Rock";
       } else {
+        updateScore("pc");
         return "You lose! Rock beat Scissors ";
       }
     case "paper":
       if (playerChoice === "scissors") {
+        updateScore("player wins");
         return "You win! Scissors beat Paper ";
       } else {
+        updateScore("pc");
         return "You lose! Paper beats Rock";
       }
     case "scissors":
-      if (playerChoice === "scissors") {
-        return "You lose! Scissors beat Paper";
-      } else {
+      if (playerChoice === "rock") {
+        updateScore("player wins");
         return "You win! Rock beats Scissors";
+      } else {
+        updateScore("pc");
+        return "You lose! Scissors beats Paper";
       }
   }
 }
 
 function game() {
   for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Your turn");
+    let playerSelection = getPlayerChoice();
     let computerSelection = getComputerChoice();
 
     console.log(playRound(playerSelection, computerSelection));
   }
+
+  console.log(`Final result: Player ${playerScore} - PC ${pcScore}`);
 }
 
 game();
